103c019202afdf322e810cfe8ce411e218c48742
na-go <kn.mbk201.mynk@gmail.com>
2022-03-07 17:48:00 +0900

    inputbarの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, VFC } from 'react'
 
-function App() {
+type Task = {
+    id: number
+    title: string
+    done: boolean
+}
+
+const initialTask:Task[] = [
+  {
+    id: 1,
+    title: 'やること',
+    done: false
+  }
+]
+
+const App: VFC = () => {
+  const [inputTask, setInputTask] = useState<string>('')
+  const [inputTitle, setInputTitle] = useState<string>(' ')
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputTitle(e.target.value)
+  }
+
+  const handleSubmit = () => {
+    setInputTask(inputTitle)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        To-Do App
       </header>
+
+      <div className='input-bar'>
+        <input
+          type="text"
+          className="input-task"
+          value={inputTitle}
+          onChange={handleInputChange}
+          placeholder="TODO入力欄"
+        />
+        <button className='input-button' onClick={handleSubmit}>追加する</button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
