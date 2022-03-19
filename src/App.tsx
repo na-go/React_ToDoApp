a1179b9febe5fd4f90a8e4e592763a4e6572cb68
na-go <kn.mbk201.mynk@gmail.com>
2022-03-19 10:49:08 +0900

    編集と削除できるようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import React, { useState, VFC } from 'react'
+import React, { useEffect, useState, VFC } from 'react'
 import { Task } from './type'
 import { InputBar } from './component/input_bar'
-import { EditModal } from './component/edit_modal'
 
 const initialTask:Task[] = [
   {
@@ -18,7 +17,8 @@ const initialTask:Task[] = [
 
 const App: VFC = () => {
   const [tasks, setTasks] = useState<Task[]>(initialTask)
-  const [inputTitle, setInputTitle] = useState<string>(' ')
+  const [inputTitle, setInputTitle] = useState<string>('')
+  const [editTitle, setEditTitle] = useState<string>('')
   const [showModal, setShowModal] = useState<Task | undefined>()
   const [countId, setCountId] = useState<number>(tasks.length + 1)
 
@@ -30,16 +30,38 @@ const App: VFC = () => {
     }
     setTasks([newTask, ...tasks])
     setInputTitle('')
-    setCountId(tasks.length + 1)
   }
 
   const handleDone = (task: Task) => {
   }
 
-  const openEdit = (task:Task) => {
-    setShowModal(task)
-  }
+  const handleEdit = (showModal: Task) => {
+      const editedTask: Task[] = tasks.map((task:Task) => {
+        if (task.id === showModal.id) {
+          const editTask:Task = {
+            id: showModal.id,
+            title: editTitle,
+            done: false
+          }
+          return editTask
+        }
+        return task
+      })
+      setTasks([...editedTask])
+      setEditTitle('')
+      setShowModal(undefined)
+    }
 
+    const handleDelete = (choosedTask:Task) => {
+      const afterDeleteTasks:Task[] = tasks.filter(task =>
+        task.id !== choosedTask.id)
+      setTasks(afterDeleteTasks)
+      setShowModal(undefined)
+    }
+
+useEffect(() => {
+  setCountId(tasks.length + 1)
+})
 
 
   return (
@@ -58,8 +80,8 @@ const App: VFC = () => {
         {tasks.length <= 0 ? '何も登録されてないよ' :
         <ul className="task-list">
           ToDo一覧だよ
-          {tasks.map((task) => (
-            <li className='task-id`${task.id}`'>
+          {tasks.map((task,key) => (
+            <li key={key} className={'task-id'}>
                 <input
                     type="checkbox"
                     className="checkbox-input"
@@ -70,7 +92,7 @@ const App: VFC = () => {
                 <span className="checkbox-label">{ task.title }</span>
               </label>
               <button
-                onClick={() => openEdit(task)}
+                onClick={() => setShowModal(task)}
                 className="btn-edit"
               >
                 編集
@@ -79,14 +101,24 @@ const App: VFC = () => {
           ))}
         </ul>
         }
-        <EditModal
-          tasks={tasks}
-          setTasks={setTasks}
-          showModal={showModal}
-          setShowModal={setShowModal}
-          inputTitle={inputTitle}
-          setInputTitle={setInputTitle}
-        />
+
+        {showModal &&
+        <div id="overlay">
+          <div id="modalContent">
+            <p>修正を記述してください</p>
+            <input
+              type="text"
+              className='edit-input'
+              value={editTitle}
+              defaultValue={showModal.title}
+              onChange={(e) => {setEditTitle(e.target.value)}}
+            />
+            <button onClick={() => handleEdit(showModal)}>変更</button>
+            <button onClick={() => handleDelete(showModal)}>削除</button>
+            <button onClick={() => setShowModal(undefined)}>やめる</button>
+          </div>
+        </div>
+      }
 
       </div>
     </div>
